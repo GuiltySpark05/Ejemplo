4a46ad318aacd3cd4f049cbccebae54b837bfb3b
Luis Javier Mercado <50953075+xluis1973@users.noreply.github.com>
2023-08-15 11:16:06 -0300

    create main

--- src/ejemplo/Ejemplo.java
+++ src/ejemplo/Ejemplo.java
@@ -5,6 +5,9 @@ public class Ejemplo {
 
    
     public static void main(String[] args) {
+        Vehiculo v=new Moto("xx","fiat",10);
+        v.avanzar(100);
+        System.out.println("quedo "+v.getCantCombustible());
         
     }
     
